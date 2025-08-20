009ff07cd7f95deedd574aa18bea54bcc2756bc2
Nerkar <2388092@cognizant.com>
2025-08-20 12:47:40 +0530

    Second commit

--- jobportal/src/main/java/com/example/jobportal/controller/JobPostActivityController.java
+++ jobportal/src/main/java/com/example/jobportal/controller/JobPostActivityController.java
@@ -1,19 +1,25 @@
 package com.example.jobportal.controller;
 
 import com.example.jobportal.entity.JobPostActivity;
+import com.example.jobportal.entity.RecruiterJobsDto;
+import com.example.jobportal.entity.RecruiterProfile;
 import com.example.jobportal.entity.Users;
 import com.example.jobportal.services.JobPostActivityService;
 import com.example.jobportal.services.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.Banner;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Date;
+import java.util.List;
 
 @Controller
 public class JobPostActivityController {
@@ -50,6 +56,10 @@ public class JobPostActivityController {
         if (!(authentication instanceof AnonymousAuthenticationToken)) {
             String currentUsername = authentication.getName();
             model.addAttribute("username", currentUsername);
+            if(authentication.getAuthorities().contains(new SimpleGrantedAuthority("Recruiter"))){
+                List<RecruiterJobsDto> recruiterJobs = jobPostActivityService.getRecruiterJobs(((RecruiterProfile) currentUserProfile).getUserAccountId());
+                model.addAttribute("jobPost", recruiterJobs);
+            }
         }
 
         model.addAttribute("user", currentUserProfile);
@@ -73,6 +83,14 @@ public class JobPostActivityController {
         model.addAttribute("jobPostActivity", jobPostActivity);
         jobPostActivityService.addNew(jobPostActivity);
         return "redirect:/dashboard/";
+    }
+
+    @PostMapping("dashboard/edit/{id}")
+    public String editJob(@PathVariable("id") int id, Model model){
+        JobPostActivity jobPostActivity = jobPostActivityService.getOne(id);
+        model.addAttribute("jobPostActivity", jobPostActivity);
+        model.addAttribute("user", usersService.getCurrentUserProfile());
+        return "add-jobs";
     }
 
 }
--- jobportal/src/main/java/com/example/jobportal/services/JobPostActivityService.java
+++ jobportal/src/main/java/com/example/jobportal/services/JobPostActivityService.java
@@ -1,8 +1,11 @@
 package com.example.jobportal.services;
 
-import com.example.jobportal.entity.JobPostActivity;
+import com.example.jobportal.entity.*;
 import com.example.jobportal.repository.JobPostActivityRepo;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class JobPostActivityService {
@@ -15,6 +18,24 @@ public class JobPostActivityService {
 
     public JobPostActivity addNew(JobPostActivity jobPostActivity){
         return jobPostActivityRepo.save(jobPostActivity);
+    }
+
+    public List<RecruiterJobsDto> getRecruiterJobs(int recruiter){
+        List<IRecruiterJobs> recruiterJobs = jobPostActivityRepo.getRecruiterJobs(recruiter);
+
+        List<RecruiterJobsDto> recruiterJobsDtoList = new  ArrayList<>();
+
+        for(IRecruiterJobs rec: recruiterJobs){
+            JobLocation loc = new JobLocation(rec.getLocationId(), rec.getCity(), rec.getState(), rec.getCountry());
+            JobCompany comp = new JobCompany(rec.getCompanyId(), rec.getName(), "");
+            recruiterJobsDtoList.add(new RecruiterJobsDto(rec.getTotalCandidates(), rec.getJob_post_id(), rec.getJob_title()
+            ,loc, comp));
+        }
+        return recruiterJobsDtoList;
+    }
+
+    public JobPostActivity getOne(int id) {
+        return jobPostActivityRepo.findById(id).orElseThrow(()-> new RuntimeException("Job not found"));
     }
 }
 
